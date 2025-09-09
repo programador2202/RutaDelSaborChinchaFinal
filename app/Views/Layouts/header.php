e9a6ce3a7a1e823bbd45596bb8898732ab928469
Aimar Alexander <Alexander2000contreras@gmail.com>
2025-09-09 16:48:31 -0500

    Actualizado

--- app/Views/Layouts/header.php
+++ app/Views/Layouts/header.php
@@ -6,6 +6,8 @@
   <title>Ruta del Sabor Chincha</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/css/navbar.css') ?>">
+
 </head>
 
 <body>
@@ -14,9 +16,9 @@
     <nav class="navbar navbar-expand-lg bg-white shadow-sm py-2">
       <div class="container">
         <!-- Logo -->
-        <a class="navbar-brand d-flex align-items-center" href="/RutaDelSaborChincha123/index.php">
-        <img src="<?= base_url('public/img/inicio_logo.png') ?>" alt="Logo Ruta del Sabor">
-        </a>
+      <a class="navbar-brand d-flex align-items-center" href="/RutaDelSaborChincha123/index.php">
+        <img src="<?= base_url('img/inicio_logo.png') ?>" alt="Logo Ruta del Sabor" class="img-fluid" style="max-height: 50px;">
+      </a>
 
         <!-- Botón hamburguesa -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
@@ -29,19 +31,21 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item mx-2">
-              <a class="nav-link" href="/RutaDelSaborChincha123/index.php"><i class="fas fa-home me-1"></i>Inicio</a>
+              <a class="nav-link" href="/RutaDelSaborChincha123/index.php"></i>Inicio</a>
             </li>
-            <li class="nav-item  mx-2">
-              <a class="nav-link" href="#categoria">
-                <i class="fas fa-utensils me-1"></i> Categorías
-              </a>
-
+      
               <li class="nav-item mx-2">
-              <a class="nav-link" href="/RutaDelSaborChincha123/views/nosotros.php"><i class="fas fa-users me-1"></i>Nosotros</a>
+              <a class="nav-link" href="/RutaDelSaborChincha123/views/nosotros.php"></i>Nosotros</a>
             </li>
+
+              <li class="nav-item  mx-2">
+              <a class="nav-link" href="#categoria">
+                Categorías
+              </a>
             </li>
-            <li class="nav-item mx-2"><a class="nav-link" href="/RutaDelSaborChincha123/views/categorias/Vitinicolas.php"><i class="fas fa-wine-bottle me-1"></i> Vitinícolas</a></li>
-            <li class="nav-item mx-2"><a class="nav-link" href="#"><i class="fas fa-blog me-1"></i> Blog</a></li>
+
+            <li class="nav-item mx-2"><a class="nav-link" href="/RutaDelSaborChincha123/views/categorias/Vitinicolas.php"> Vitinícolas</a></li>
+            <li class="nav-item mx-2"><a class="nav-link" href="#"> Blog</a></li>
             <li class="nav-item mx-2"><a class="nav-link" href="#"><i class="fab fa-facebook fa-lg"></i></a></li>
             <li class="nav-item mx-2"><a class="nav-link" href="#"><i class="fab fa-whatsapp fa-lg"></i></a></li>
             <li class="nav-item mx-2"><a class="nav-link" href="#"><i class="fab fa-tiktok fa-lg"></i></a></li>
